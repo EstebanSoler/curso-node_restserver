d0e0345826e6279f9a98ce47f4349882d03a914d
chechi <alfredo.soler@globant.com>
2022-04-12 01:25:21 -0300

    file upload added

--- models/server.js
+++ models/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('../database/config');
+const fileUpload = require('express-fileupload');
 
 
 class Server {
@@ -15,6 +16,7 @@ class Server {
       categorias: '/api/categorias',
       productos: '/api/productos',
       usuarios: '/api/usuarios',
+      uploads: '/api/uploads'
     }
 
 
@@ -34,14 +36,21 @@ class Server {
 
   middleware() {
 
-    //CORS
+    // CORS
     this.app.use(cors())
 
-    //Lectura y parse del body
+    // Lectura y parse del body
     this.app.use(express.json());
 
-    //directorio publico
+    // directorio publico
     this.app.use( express.static('public'));
+
+    // FileUpload
+    this.app.use(fileUpload({
+      useTempFiles : true,
+      tempFileDir : '/tmp/',
+      createParentPath: true,
+    }));
   }
 
   routes(){
@@ -50,6 +59,7 @@ class Server {
     this.app.use( this.paths.categorias, require('../routes/categorias'));
     this.app.use( this.paths.productos, require('../routes/productos'));
     this.app.use( this.paths.usuarios, require('../routes/usuarios'));
+    this.app.use( this.paths.uploads, require('../routes/uploads'));
   }
 
   listen() {
